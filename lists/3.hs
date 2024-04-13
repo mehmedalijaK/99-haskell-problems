d070825d7afdd63c5e0dde038ba1910222bf8908
Mehmedalija <rg.mehmedalija@gmail.com>
2024-04-14 01:40:49 +0200

    add new implementation for problem 3

--- lists/3.hs
+++ lists/3.hs
@@ -3,8 +3,13 @@
 -- 2
 
 elementAt :: [a] -> Int -> a
-elementAt [] 0 = error "Out of bounds"
-elementAt x 0 = head x
+elementAt [] 1 = error "Out of bounds"
+elementAt x 1 = head x
 elementAt (x:xs) n 
-            | n < 0 = error "Out of bounds"
+            | n < 1 = error "Out of bounds"
             | otherwise = elementAt xs $ n - 1
+
+elementAt' :: [a] -> Int -> a
+elementAt' xxs n 
+            | n < 1 || n > length xxs = error "Out of bounds"
+            | otherwise = last (take n xxs)
